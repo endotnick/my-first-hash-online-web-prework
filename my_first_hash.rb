e7ef8b2aba6f43adb660f3ad0c31d379933864f5
Nick Rodriguez <nickbenrodriguez@gmail.com>
2018-11-21 05:20:05 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,3 +1,4 @@
+
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 {"key" => "val"}
@@ -19,8 +20,8 @@ def retrieval
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+  shipping_manifest['oil paintings']
 
 end
 
